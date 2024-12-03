27abbbb7b31a0c6c7003f528ce4b998bc49469f1
vais2004 <vkawale2004@gmail.com>
2024-12-03 15:53:05 +0530

    update

--- App.jsx
+++ App.jsx
@@ -1,2 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js"
+
+export default function App(){
+    return(
+        <>
+        <main>
+            
+        </main>
+        </>
+    )
+}
